5ef882ca7feca8117fccd72200c795ad757e90fd
yazj <yuanyi2000@protonmail.com>
2024-06-18 12:24:08 +0800

    player basic movement

--- src/Game/EventHandler/input_handler.gd
+++ src/Game/EventHandler/input_handler.gd
@@ -1,8 +1,18 @@
 class_name InputHandler
 extends Node
 
-func get_action() -> Action:
+const directions = {
+	"move_up": Vector2i.UP,
+	"move_down": Vector2i.DOWN,
+	"move_left": Vector2i.LEFT,
+	"move_right": Vector2i.RIGHT,
+}
+
+func get_action(character: Character) -> Action:
 	var action: Action = null
 	# movement action
-	
+	for direction in directions:
+		if Input.is_action_pressed(direction):
+			var offset: Vector2i = directions[direction]
+			action = MoveAction.new(character, offset.x, offset.y)
 	return action
--- src/Game/game.gd
+++ src/Game/game.gd
@@ -7,7 +7,7 @@ extends Node2D
 @onready var input_handler: InputHandler = $InputHandler
 @onready var camera: Camera2D = $Camera2D
 
-var player_pos: Vector2i
+var player_grid_pos: Vector2i
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,57 +17,20 @@ func _ready():
 	player.add_child(camera)
 	
 	characters.add_child(player)
-	player_pos = player.grid_position 
 	
-#func _input(event):
-	#
-	#if Input.is_action_just_pressed("ui_up"):
-		#move_action = MoveAction.new(player, 0, -1)
-		#player_pos += move_action.offset
-		#player.position = Grid.grid_to_world(player_pos)
-	#elif Input.is_action_just_pressed("ui_down"):
-		#move_action = MoveAction.new(player, 0, 1)
-		#player_pos += move_action.offset
-		#player.position = Grid.grid_to_world(player_pos)
-	#elif Input.is_action_just_pressed("ui_left"):
-		#move_action = MoveAction.new(player, -1, 0)
-		#player_pos += move_action.offset
-		#player.position = Grid.grid_to_world(player_pos)
-	#elif Input.is_action_just_pressed("ui_right"):
-		#move_action = MoveAction.new(player, 1, 0)
-		#player_pos += move_action.offset
-		#player.position = Grid.grid_to_world(player_pos)
-	#else:
-		#return
-	#
-	
-var i: float = 0;
-var speed: float = 0.7;
+	player_grid_pos = Grid.world_to_grid(player.position)
 
+var can_move: bool = true
 func _physics_process(delta) -> void:
+	var action: Action = await input_handler.get_action(player)
 	
-	i += speed;
-	if i >= 10:
-		if Input.is_action_pressed("ui_up"):
-			move_action = MoveAction.new(player, 0, -1)
-			player_pos += move_action.offset
-			player.position = Grid.grid_to_world(player_pos)
-		elif Input.is_action_pressed("ui_down"):
-			move_action = MoveAction.new(player, 0, 1)
-			player_pos += move_action.offset
-			player.position = Grid.grid_to_world(player_pos)
-		elif Input.is_action_pressed("ui_left"):
-			move_action = MoveAction.new(player, -1, 0)
-			player_pos += move_action.offset
-			player.position = Grid.grid_to_world(player_pos)
-		elif Input.is_action_pressed("ui_right"):
-			move_action = MoveAction.new(player, 1, 0)
-			player_pos += move_action.offset
-			player.position = Grid.grid_to_world(player_pos)
-		else:
-	
-			return
-		i = 0
-	
-	
+	if action is MoveAction and can_move:
+		can_move = false
+		player_grid_pos += action.offset
+		var dest = Vector2(Grid.grid_to_world(player_grid_pos))
+		var tween = create_tween()
+		tween.tween_property(player, "position", dest, 0.2)
+		await tween.finished
+		can_move = true
+
 
